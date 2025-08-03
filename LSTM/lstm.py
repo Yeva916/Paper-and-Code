747c8ec8737a7572aee493dafe6750808eec962c
Yeshwant Kuriyavar <yeshwa916@gmail.com>
2025-08-04 03:12:53 +0530

    Lstm code competeled

--- LSTM/lstm.py
+++ LSTM/lstm.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from typing import Optional, Tuple
 
 class LSTMCell(nn.Module):
     def __init__(self,input_size:int,hidden_size:int,layer_norm:bool):
@@ -41,3 +42,42 @@ output ->
 {'c_next': tensor([-0.4601,  1.1984,  2.5017]), 'h_next': tensor([-0.6833,  0.0275,  0.3205])}
 
 '''
+
+class LSTM(nn.Module):
+    def __init__(self,input_size:int,hidden_size:int,n_layers:int):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        self.cells = nn.ModuleList([LSTMCell(input_size,hidden_size)] +
+                                   [LSTMCell(input_size,hidden_size) for _ in range(self.n_layer - 1)])
+
+    def forward(self,x:torch.Tensor,state:Optional[Tuple[torch.Tensor,torch.Tensor]]):
+
+        # x = [num_steps,batch_size,input_size]
+
+        num_steps,batch_size = x[:2]
+
+        if state is None:
+            h = [x.new_zeros(batch_size,self.hidden_size) for _ in range(self.n_layer)]
+            c = [x.new_zeros(batch_size,self.hidden_size) for _ in range(self.n_layer)]
+        else:
+            h,c = state
+        
+        h,c = list(h),list(c)
+
+        out = []
+
+        for t in range(num_steps):
+
+            inp = x[t]
+
+            for layer in self.n_layers:
+                h[layer],c[layer] = self.cells[layer](inp,h[layer],c[layer])
+
+                inp = h[layer]
+
+            out.append(h[-1])
+        out = torch.stack(out)
+        h = torch.stack(h)
+        c = torch.state(c)
+        return out,(h,c)
